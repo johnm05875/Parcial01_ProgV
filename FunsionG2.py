416dbe9f77543bcc72191e7c2e013b539c5e1af1
John Miró <john.miiiro@gmail.com>
2026-02-09 09:46:12 -0500

    Problema2. Version2. John Miró

--- FunsionG2.py
+++ FunsionG2.py
@@ -1,6 +1,8 @@
 # Suma de una lista de números
-# Desarrollado por: freinanyelis gomez
-# Función que recibe una lista de números y calcula la suma total
+#*********************************
+#** Desarrollado por: Freinanyelis Gomez        ID: 159935684 **
+#*****************************************
+
 def suma_lista(numeros):
    total = sum(numeros)
    return total
@@ -11,3 +13,19 @@ entrada = input("Ingrese varios números separados por espacio: ")
 lista_numeros = list(map(int, entrada.split()))
 resultado = suma_lista(lista_numeros)
 print("freinanyelis gomez - Resultado de la función:", resultado)
+
+#Promedio de-lista
+#*********************************
+#** Desarrollado por: John Miró  ID: 10-715-2197 **
+#******************************************************
+
+def calcular_promedio(lista):
+    if len(lista) == 0:
+        return 0  # Evita división entre cero
+    return sum(lista) / len(lista)
+
+
+# Ejemplo de uso
+numeros = [10, 8, 9, 7, 6]
+promedio = calcular_promedio(numeros)
+print("El promedio es:", promedio)
