920a368824fae2f5960e01a509b5c810ce3f4e60
John Miró <john.miiiro@gmail.com>
2026-02-09 09:38:57 -0500

    John Miró

--- FunsionG1.py
+++ FunsionG1.py
@@ -1,9 +1,8 @@
 # Par - Impar
+#*********************************
+#** Desarrollado por: Freinanyelis Gomez        ID: 159935684 **
 
-# Desarrollado por: Héctor Lavoe
-
-# Función que recibe un número entero y determina si es par o impar
-
+#*****************************************
 def par_impar(numero):
 
     if numero % 2 == 0:
@@ -22,4 +21,27 @@ numero = int(input("Ingrese un número entero: "))
 resultado = par_impar(numero)
 
 print("Héctor Lavoe - Resultado de la función:", resultado)
+
  
+ 
+#Mayor-numero
+#*********************************
+#** Desarrollado por: John Miró  ID: 10-715-2197 **
+
+#*****************************************
+def mayor_de_dos(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+
+# Ejemplo de uso
+a = int(input("Ingrese el primer número: "))
+b = int(input("Ingrese el segundo número: "))
+
+resultado = mayor_de_dos(a, b)
+print("El número mayor es:", resultado)
+
+
+
