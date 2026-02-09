0bc30d6c3c9c6a68ea6ef823b14ededf0e41c61a
freinanyelis gomez <freinanyelis@gmail.com>
2026-02-09 10:24:42 -0500

    problema 7 version 1 freinanyelis gomez

---
+++ FunsionG7.py
@@ -0,0 +1,10 @@
+# Lista Mayúscula - Lista Minúscula
+# Desarrollado por: freinanyelis gomez ID 159935684
+# Función que recibe una cadena de texto y la retorna en mayúscula
+def convertir_mayusculas(cadena):
+   return cadena.upper()
+
+# *********** PRUEBA DE FUNCIONAMIENTO ***********
+texto = input("Ingrese una cadena de texto: ")
+resultado = convertir_mayusculas(texto)
+print("freinanyelis gomez- Resultado de la función:", resultado)
