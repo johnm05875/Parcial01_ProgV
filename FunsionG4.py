c6bd86e82cec4a31df7296c2a71df9c8f63a635a
John Miró <john.miiiro@gmail.com>
2026-02-09 10:13:20 -0500

    Problema4. Version2. John Miro

--- FunsionG4.py
+++ FunsionG4.py
@@ -1,6 +1,7 @@
-# Lista - Número - Lista contenido
-# Desarrollado por: freinanyelis gomez
 # Función que recibe una lista y retorna el número de elementos que contiene
+#*********************************
+#** Desarrollado por: Freinanyelis Gomez        ID: 159935684 **
+#*****************************************
 def contar_elementos(lista):
    return len(lista)
 
@@ -9,3 +10,18 @@ entrada = input("Ingrese elementos separados por espacio: ")
 lista_elementos = entrada.split()
 resultado = contar_elementos(lista_elementos)
 print("freinanyelis gomez - Resultado de la función:", resultado)
+
+#Lista-contenido
+#*********************************
+#** Desarrollado por: John Miró  ID: 10-715-2197 **
+#******************************************************
+def valor_minimo(lista):
+    if len(lista) == 0:
+        return None  # Evita error si la lista está vacía
+    return min(lista)
+
+
+# Ejemplo de uso
+numeros = [5, 3, 8, 1, 9]
+minimo = valor_minimo(numeros)
+print("El valor mínimo es:", minimo)
